c62daf8c7a0dd18dfd0a39aa8203066302bf7b87
Abhishek Gupta <abhishekgupta2248@gmail.com>
2024-04-18 00:37:41 +0530

    initial commit

---
+++ cab-book-driver/cab-book-driver/src/main/java/com/springboot/cabbookdriver/constant/AppConstant.java
@@ -0,0 +1,6 @@
+package com.springboot.cabbookdriver.constant;
+
+public class AppConstant {
+
+    public static final String CAB_LOCATION = "cab-location";
+}
---
+++ cab-book-driver/cab-book-driver/src/main/java/com/springboot/cabbookdriver/service/CabLocationService.java
@@ -0,0 +1,20 @@
+package com.springboot.cabbookdriver.service;
+
+
+import com.springboot.cabbookdriver.constant.AppConstant;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public boolean updateLocation(String location) {
+        kafkaTemplate.send(AppConstant.CAB_LOCATION, location);
+        return true;
+    }
+}
